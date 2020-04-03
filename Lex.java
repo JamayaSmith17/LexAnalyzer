84fcfa6be2f3cdc3c16c47de0fb0328b9d3fc937
lepidopterane-atsmith <sabowitz@smith.edu>
2020-04-03 17:37:15 -0400

    everything compiles now

--- Lex.java
+++ Lex.java
@@ -3,147 +3,148 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 /* CSC266 Programming Assignment 1
-*
-* Bushra Tasneem
-* Sarah Abowitz
-* Jamaya (May) Smith
-*
-*  Language Name: CrystalGem
-*
-* Program for lexical analyzer
-*
-*/
+ *
+ * Bushra Tasneem
+ * Sarah Abowitz
+ * Jamaya (May) Smith
+ *
+ *  Language Name: CrystalGem
+ *
+ * Program for lexical analyzer
+ *
+ */
 
 /*
-* The purpose of this class is to print the output of getToken(token) to stdout.
-*/
+ * The purpose of this class is to print the output of getToken(token) to stdout.
+ */
 public class Lex {
-  boolean commentStarted = false;
+    boolean commentStarted = false;
 
-  public static void main(){
-    // we may not need this? wild if true
-    //
-    // for each lexeme:
-    //   if (commentStarted = false)
-    // Token token = getToken(lexeme)
-    // list.add(token.value)
-    // else getToken(lexeme)
-    // if commentStarted true throw LexicalException
-  }
-
-  // returns the next token in the input as an object with a token type and a token value
-  public CrystalToken getToken(String token) {
-    // StringTokenizer inputTokens = new StringTokenizer(token);
-    //
-    // while (inputTokens.hasMoreTokens()) {
-    //
-    //   String token2 = inputTokens.nextToken();
-
-    String determinedValue = token;
-    String determinedTokenType = tokenType(token);
-
-    if (determinedTokenType.equals("error")) { // default case - lexeme does not match any defined types
-      throw LexicalException("Lexical error identified.");
-      return null;
+    public static void main(){
+      // we may not need this? wild if true
+      //
+      // for each lexeme:
+      //   if (commentStarted = false)
+      // Token token = getToken(lexeme)
+      // list.add(token.value)
+      // else getToken(lexeme)
+      // if commentStarted true throw LexicalException
     }
 
-    newToken = new CrystalToken(determinedValue, determinedTokenType);
-    //    Test().printOutput(newToken);
+    // returns the next token in the input as an object with a token type and a token value
+    public CrystalToken getToken(String token) {
+		// StringTokenizer inputTokens = new StringTokenizer(token);
+		//
+		// while (inputTokens.hasMoreTokens()) {
+		//
+		//   String token2 = inputTokens.nextToken();
 
-    //  }
-    return newToken;
-  }
+		String determinedValue = token;
+		String determinedTokenType = tokenType(token);
 
-  // determines the type of token
-  public String tokenType(String token) {
-    String result = "";
+		if (determinedTokenType.equals("error")) { // default case - lexeme does not match any defined types
+      System.out.println("Lexical error identified."); 
+			return null;
+		}
 
-    //Bushra doing identifiers, constant numbers and constant strings
+		CrystalToken newToken = new CrystalToken(determinedValue, determinedTokenType);
+		//    Test().printOutput(newToken);
 
-    // identifiers
-    char firstChar = token.charAt(0);
-    if (Character.isLetter(firstChar)){
-      boolean isId = true;
-      for (int i=1;i<token.length(); i++){
-        if (Character.isJavaIdentifierPart(token.charAt(i)) == false){
-          isId = false;
-        }
-        if (isId){return "<Identifier>";}
-      }
+		//  }
+
+		return newToken;
     }
 
-    // constant numbers
-    firstNum = token.charAt(0);
-    subToken = token.substring(1);
-    // separately check the first character of the token
-    if ( ( Character.isDigit(firstNum) ) || ( firstNum.equals("-")) ) {
-      boolean negDigFirst = true;
-      boolean correctDot = subToken.indexOf(".")==subToken.lastIndexOf(".");
-      subToken.replace(".","");
-      boolean isNum = true;
-      if (correctDot==true){
-        for (int i=0;i<subToken.length(); i++){
-          if (Character.isDigit(subToken.charAt(i)) == false){
-            isNum = false;
+    // determines the type of token
+    public String tokenType(String token) {
+      String result = "";
+
+      //Bushra doing identifiers, constant numbers and constant strings
+
+      // identifiers
+      char firstChar = token.charAt(0);
+      if (Character.isLetter(firstChar)){
+          boolean isId = true;
+          for (int i=1;i<token.length(); i++){
+          if (Character.isJavaIdentifierPart(token.charAt(i)) == false){
+            isId = false;
           }
-          if (isNum){return "<Constant Number>";}
-        }
-
+          if (isId){return "<Identifier>";}
+          }
       }
-    }
 
-    //initialize a boolean negdigfirst that is true if first char of token is "-" or a digit
-    //count the number of dots in the token
-
-    //initialize a boolean correctdot equals true if dotcount is 0 or 1
-    // if negdigfirst & correctdot are both true, we iterate over the rest of the token
-    //and check if each char is a digit
-
-    // constant numbers should have a minimum value of -32,768 and a max val of 32767
-
-    /* while (onlyASCII == true && loopOver == true){
-    x = char at current index
-
-    boolean isASCII = null;
-    isASCII = getASCII(x);
-    if (isASCII == false){
-    onlyASCII = false;
-
-
-    if index < arr length, index++
-    else loopOver = false
-    // } */
-
-    //constant strings
-    String[] ASCII = {" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+",
-    ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8",
-    "9", ":", ";", "<", "=", ">", "?", "@", "A-Z", "a-z",
-    "[", "\\", "]", "^", "_", "`", "{", "}", "|", "~"  };
-
-    String[] stringArr = null;
-    //Use .split to split token
-    firstC = token.charAt(0);
-    lastC = token.charAt(-1);
-    if (firstC.equals('\"') && lastC.equals('\"')){
-      int tokenEnd = token.length() - 1;
-      String miniToken = token.substring(1,tokenEnd);
-      stringArr = miniToken.split();
-      //boolean isConsString = true;
-      int indS = 0;
-      boolean onlyASCII = true;
-      boolean loopOver = false;
-      boolean isASCII = null;
-      String x = null;
-
-      while (onlyASCII == true && loopOver == false){
-        x = stringArr[indS];
-        isASCII = CrystalToken.getASCII(x);
-        if (isASCII == false){
-          onlyASCII = false; }
-          if (indS < stringArr.length) {indS++;}
-          else { loopOver = true;}
+      // constant numbers
+      Character firstNum = token.charAt(0);
+      String subToken = token.substring(1);
+      // separately check the first character of the token
+      if ( ( Character.isDigit(firstNum) ) || ( firstNum.equals("-")) ) {
+          boolean negDigFirst = true;
+          boolean correctDot = subToken.indexOf(".")==subToken.lastIndexOf(".");
+          subToken.replace(".","");
+          boolean isNum = true;
+          if (correctDot==true){
+        for (int i=0;i<subToken.length(); i++){
+            if (Character.isDigit(subToken.charAt(i)) == false){
+          isNum = false;
+            }
+            if (isNum){return "<Constant Number>";}
         }
-        if (onlyASCII){ return "<Constant String>";}
+
+          }
+      }
+
+      //initialize a boolean negdigfirst that is true if first char of token is "-" or a digit
+      //count the number of dots in the token
+
+      //initialize a boolean correctdot equals true if dotcount is 0 or 1
+      // if negdigfirst & correctdot are both true, we iterate over the rest of the token
+      //and check if each char is a digit
+
+      // constant numbers should have a minimum value of -32,768 and a max val of 32767
+
+      /* while (onlyASCII == true && loopOver == true){
+            x = char at current index
+
+        boolean isASCII = null;
+            isASCII = getASCII(x);
+            if (isASCII == false){
+            onlyASCII = false;
+
+
+            if index < arr length, index++
+            else loopOver = false
+      // } */
+
+      //constant strings
+      String[] ASCII = {" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+",
+            ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+            "9", ":", ";", "<", "=", ">", "?", "@", "A-Z", "a-z",
+            "[", "\\", "]", "^", "_", "`", "{", "}", "|", "~"  };
+
+      
+      //Use .split to split token
+      Character firstC = token.charAt(0);
+      Character lastC = token.charAt(-1);
+      if (firstC.equals('\"') && lastC.equals('\"')){
+          int tokenEnd = token.length() - 1;
+          String miniToken = token.substring(1,tokenEnd);
+          String[] stringArr = miniToken.split("");
+          //boolean isConsString = true;
+          int indS = 0;
+          boolean onlyASCII = true;
+          boolean loopOver = false;
+          boolean isASCII;
+          String x = null;
+
+          while (onlyASCII == true && loopOver == false){
+        x = stringArr[indS];
+        isASCII = getASCII(x);
+        if (isASCII == false){
+            onlyASCII = false; }
+        if (indS < stringArr.length) {indS++;}
+        else { loopOver = true;}
+          }
+          if (onlyASCII){ return "<Constant String>";}
 
       }
 
@@ -151,8 +152,8 @@ public class Lex {
 
       // Checking if token is a keyword
       String[] keywords = { "number", "string", "vector" ,"print" , "strcat",
-      "true", "false" , "function" , "return" , "void",
-      "main" , "if" , "else" , "while" , "null" };
+                "true", "false" , "function" , "return" , "void",
+                "main" , "if" , "else" , "while" , "null" };
 
       for (String k : keywords){
         if (token.equals(k)){return "<Keyword>";}
@@ -166,76 +167,66 @@ public class Lex {
 
       // Sarah covering whitespace + comments
       String[] operators = {"=", "+", "-", "/", "*", "<", ">", "<=", ">=", "==",
-      "!=", "!" , "&&", "||"};
+                "!=", "!" , "&&", "||"};
 
       for (String o : operators){
         if (token.equals(o)){return "<Operator>";}
       }
 
       if (token.startsWith("/*")){
-        commentStarted = true;
-        return "<Comment>";
+          commentStarted = true;
+          return "<Comment>";
       }
 
       if (token.endsWith("*/")){
-        commentStarted = false;
-        return "<>";
+          commentStarted = false;
+          return "<>";
       }
-
       else {
-
-        Pattern pattern = Pattern.compile( "[ \t\n\f\r]" );
+        Pattern pattern = Pattern.compile("[ \t\n\f\r]" );
         Matcher matcher = pattern.matcher(token);
         boolean found = matcher.find();
 
         if ( found = matcher.find() == true ) {
-          result = "";
-        }
-        else {
+          return "<Whitespace>";
+        } else {
           result = "error";
         }
-
+        
         return result;
       }
-    }
+	}
 
-    // token class of CrystalGem
-    public class CrystalToken {
-      private String value;
-      private String tokenType;
+	public boolean getASCII(String s){
+		String[] ASCII = {" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+",
+				",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+				"9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G",
+				"H", "I", "J", "K", "L", "M","N", "O", "P", "Q", "R", "S", "T", "U", "V" ,
+				"W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+				"j", "k", "l", "m", "n", "o", "p", "q", "r",
+				"s", "t", "u", "v", "w", "x", "y", "z", "[", "\\", "]",
+				"^", "_", "`", "{", "}", "|", "~" };
+		for(String a: ASCII){ 
+			if ( s.equals(a) ){
+				return true;
+			}	
+		}
+		return false;
+	}
 
-      public CrystalToken(String v, String t){
+	// token class of CrystalGem
+	public class CrystalToken {
+	    private String value;
+	    private String tokenType;
+
+	    public CrystalToken(String v, String t){
         value = v;
         tokenType = t;
       }
 
-      public String getValue(){ return value; }
+	    public String getValue(){ return value; }
 
-      public String getTokenType(){ return tokenType; }
+	    public String getTokenType(){ return tokenType; }		
+	}
 
-      public boolean getASCII(String s){
-        String[] ASCII = {" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+",
-        ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8",
-        "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G",
-        "H", "I", "J", "K", "L", "M","N", "O", "P", "Q", "R", "S", "T", "U", "V" ,
-        "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
-        "j", "k", "l", "m", "n", "o", "p", "q", "r",
-        "s", "t", "u", "v", "w", "x", "y", "z", "[", "\\", "]",
-        "^", "_", "`", "{", "}", "|", "~" };
-        for(String a: ASCII){
-          if ( s.equals(a) ){
-            return true;
-          }
-        }
-
-      }
-    }
-
-
-    class LexicalException extends Exception {
-      public LexicalException(String message){
-        super(message);
-      }
-    }
-
-  } // End of class Lex()
+} // End of class Lex()
--- Test.java
+++ Test.java
@@ -3,69 +3,73 @@ import java.io.FileReader;
 import java.util.StringTokenizer;
 
 /* CSC266 Programming Assignment 1
-*
-* Bushra Tasneem
-* Sarah Abowitz
-* Jamaya (May) Smith
-*
-*  Language Name: CrystalGem
-*
-* Program to test lex analyzer
-*
-*/
+ *
+ * Bushra Tasneem
+ * Sarah Abowitz
+ * Jamaya (May) Smith
+ *
+ *  Language Name: CrystalGem
+ *
+ * Program to test lex analyzer
+ *
+ */
 
 /*
-* This purpose of this class is to test our lexical analyzer
-*/
+ * This purpose of this class is to test our lexical analyzer
+ */
 public class Test {
 
+    public static void printOutput(Lex.CrystalToken output) {
+		String tokenVal = output.getTokenType();
+		String tokenTy = output.getValue();
 
-  public void printOutput(CrystalToken output) {
-    String tokenVal = output.determinedValue;
-    String tokenTy = output.determinedTokenType;
-
-      // print output from getToken()
-    System.out.println("Token type: " + tokenTy + "Token value: " + tokenVal );
-
-  }
-
-  // reads the input file from the command line, line by line, and passes them to Lex()
-  public static void main(String[] args) {
-    boolean commentStarted = false;
-    String filename = args[0];
-    BufferedReader reader = new BufferedReader(new FileReader(filename));
-    String tokenLine = reader.readLine();
-
-
-    while (tokenLine != null) {
-      tokenLine = reader.readLine();
-      System.out.println(tokenLine); //debugging
-
-      StringTokenizer newTokenLine = new StringTokenizer(tokenLine);
-
-      while (newTokenLine.hasMoreTokens()) {
-      // passes line
-        if (commentStarted == false){
-          CrystalToken token = Lex.getToken(newTokenLine.nextToken());
-          printOutput(token);
-          if (token.getTokenType().equals("<Comment>")){
-            commentStarted = true;
-          } else if (token.getTokenType().equals("<>")) {
-            throw Lex.LexicalException("comment found with no start");
-          }
-        } else {
-          // comment started, anything that's not an end is not a code bit.
-          CrystalToken token = Lex.getToken(newTokenLine.nextToken());
-          if (token.getTokenType().equals("<>")){
-            commentStarted = false;
-          }
-        }
-
-      if (commentStarted){
-        throw Lex.LexicalException("unfinished comment found");
-       }
-     }
-
+		// print output from getToken()
+		System.out.println("Token type: " + tokenTy + "Token value: " + tokenVal );
     }
-  }
+
+    // reads the input file from the command line, line by line, and passes them to Lex()
+    public static void main(String[] args) {
+		Lex crystalLex = new Lex();
+		boolean commentStarted = false;
+		String filename = args[0];
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String tokenLine = reader.readLine();
+
+			while (tokenLine != null) {
+				tokenLine = reader.readLine();
+				System.out.println(tokenLine); //debugging
+
+				StringTokenizer newTokenLine = new StringTokenizer(tokenLine);
+
+				while (newTokenLine.hasMoreTokens()) {
+					// passes line
+					if (commentStarted == false){
+						Lex.CrystalToken token = crystalLex.getToken(newTokenLine.nextToken());
+						printOutput(token);
+						
+						if (token.getTokenType().equals("<Comment>")){
+						commentStarted = true;
+						} else if (token.getTokenType().equals("<>")) {
+						System.out.println("comment found with no start");
+						}
+					} else {
+						// comment started, anything that's not an end is not a code bit.
+						Lex.CrystalToken token = crystalLex.getToken(newTokenLine.nextToken());
+						if (token.getTokenType().equals("<>")){
+						commentStarted = false;
+						}
+					}
+
+					if (commentStarted){
+						System.out.println("Unfinished comment found.");
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+    }
 } // End of Test()
